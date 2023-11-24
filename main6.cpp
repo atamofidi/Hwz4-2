bedb5afc5f9e34c91047ec8f1d772349f949c609
atamofidi <151543259+atamofidi@users.noreply.github.com>
2023-11-24 11:27:27 -0800

    Add files via upload

---
+++ main6.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+        
+        float d ,b,  a , x , y, z;
+        
+        
+        cin >> x >> y >> z;
+        
+        a=((x-y)*(x+z));
+
+        b=x*x+y*y+z*z+2*x*y+2*y*z+2*z*x+1;
+ 
+        d=a/b;
+   
+
+
+   cout<<d;	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
